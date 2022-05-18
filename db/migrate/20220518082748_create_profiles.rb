66612bbb12e6a6ce41faf7c248a2d89a3e6d7fa6
kapilchoudhary <kchoudhary006@gmail.com>
2022-05-18 14:01:00 +0530

    fixed profile model

--- db/migrate/20220518082748_create_profiles.rb
+++ db/migrate/20220518082748_create_profiles.rb
@@ -1,10 +1,15 @@
 class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
     create_table :profiles do |t|
+      t.references :user, null: false, foreign_key: true
       t.string :first_name
+      t.string :string
       t.string :last_name
-      t.integer :type
-      t.integer :exprience
+      t.string :string
+      t.string :user_type
+      t.string :integer
+      t.string :experience
+      t.string :integer
       t.string :description
       t.string :text
       t.string :hourly_rate
