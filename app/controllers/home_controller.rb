3803936c6100db39d1eed3819a4056b1b31f4444
kapilchoudhary <kchoudhary006@gmail.com>
2022-05-19 19:36:24 +0530

    filter latest freelancer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @freelancers = Profile.freelancer_user.order('feature DESC')
-    @pagy, @freelancers = pagy(@freelancers, items: 1)
+    @freelancers = Profile.latest_frelancer.order('feature DESC')
+    @pagy, @freelancers = pagy(@freelancers, items: 10)
   end
 
   def filter
-    freelancer = Profile.filter_rate(params[:min_value], params[:max_value])
-    pagy, freelancer = pagy(freelancer, items: 1)
-    render partial: 'devise/shared/show', locals: {freelancers: freelancer, pagy: pagy}
+    freelancers = Profile.filter_rate(params[:min_value], params[:max_value]).order('feature DESC')
+    pagy, freelancer_list = pagy(freelancers, items: 10)
+    render partial: 'devise/shared/show', locals: {freelancers: freelancer_list, pagy: pagy}
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,9 +5,11 @@ class Profile < ApplicationRecord
 
   scope :freelancer_user, -> { where(user_type: 'freelancer') }
 
-  scope :filter_rate, ->(start_amount, end_amount) {
-    where('hourly_rate BETWEEN ? AND ?', start_amount, end_amount)
+  scope :filter_rate, ->(min_value, max_value) {
+    where('hourly_rate BETWEEN ? AND ?', min_value, max_value)
   }
+
+  scope :latest_frelancer, -> { where(created_at: 24.hours.ago..Time.now)}
 
   def full_name
     "#{first_name} #{last_name}"
