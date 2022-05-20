f3a3877487a025513aad4d41d768846d7548396b
kapilchoudhary <kchoudhary006@gmail.com>
2022-05-20 14:30:59 +0530

    add caching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,20 @@
 class HomeController < ApplicationController
   def index
-    @freelancers = Profile.latest_frelancer.order('feature DESC')
+    @freelancers = fetch_freelancers
     @pagy, @freelancers = pagy(@freelancers, items: 10)
   end
 
   def filter
     freelancers = Profile.filter_rate(params[:min_value], params[:max_value]).order('feature DESC')
     pagy, freelancer_list = pagy(freelancers, items: 10)
-    render partial: 'devise/shared/show', locals: {freelancers: freelancer_list, pagy: pagy}
+    render partial: 'devise/shared/show', locals: {freelancers: freelancer_list, pagy: pagy }
+  end
+
+  private
+
+  def fetch_freelancers
+    Rails.cache.fetch("no-filter", expires_in: 24.hours) do
+      Profile.freelancer_user.order('feature DESC')
+    end
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -10,9 +10,7 @@ class Profile < ApplicationRecord
   scope :filter_rate, ->(min_value, max_value) {
     where('hourly_rate BETWEEN ? AND ?', min_value, max_value)
   }
-
-  scope :latest_frelancer, -> { where(created_at: 24.hours.ago..Time.now)}
-
+  
   def full_name
     "#{first_name} #{last_name}"
   end
